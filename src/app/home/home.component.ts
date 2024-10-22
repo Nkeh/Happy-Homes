9812388fc7bc53962c7b25c08010c5bc9df767ef
Nkeh <bonyankeh@gmail.com>
2024-10-22 09:01:56 +0100

    Done with turorials

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,8 +43,12 @@ export class HomeComponent {
   }
 
   constructor () {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+    // this.housingLocationList = this.housingService.getAllHousingLocations();
+    // this.filteredLocationList = this.housingLocationList;
   }
 
 
